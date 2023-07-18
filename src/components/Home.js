0fd7e4c3732a22a3072bb820db5d521cefa45162
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2023-07-18 20:48:19 +0530

    navbar created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import '../style/Home.css';
+import PhoneIcon from '../assets/Call-Ringing.svg'
+
 
 function Home() {
+    const menuItems = ["Home", "About", "Loans", "FAQs", "Contact"];
   return (
-    <div>Home</div>
-  )
+    <>
+      {/* top section for heading */}
+      <section className="top-heading">
+        <div className="heading-info">Your Partner in Financial Success</div>
+      </section>
+
+      {/* section for links heading  */}
+      <section className="links-heading">
+        {/* site name  */}
+        <div className="site-name">
+        DPC
+        </div>
+
+        {/* navigation links  */}
+        <ul className="nav-links">
+        {menuItems.map((link, index)=><li key={index}>{link}</li>)}
+        </ul>
+        
+
+        {/* contacts and logins */}
+        <div className="contact-section">
+          <div className='phone-logo'>
+            <img src={PhoneIcon} alt='phone icon'/>
+          </div>
+          <button className='button-login'>Login</button>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
